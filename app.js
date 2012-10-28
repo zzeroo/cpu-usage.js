8c4444f9cdef44db3422b66583fba4c0e1ab3679
zzeroo <co@zzeroo.com>
2012-10-28 12:20:23 +0100

    restore working version for public git repo

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
-  , spawn = require('child_process').spawn
-  , $p = require('procstreams');
+  , spawn = require('child_process').spawn;
 
 server.listen(3000);
 
@@ -10,11 +9,8 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
 
-setInterval(function() {
-  spawn('grep', ['^cpu\ ', '/proc/stat'])
-    .stdout.on('data', function(data){
-      var txt = new Buffer(data).toString('utf8', 0, data.length);
-      console.log('stdout: ' + data);
-    });
-}, 1000);
-
+var com = spawn('dstat', ['-c', '--nocolor']);
+com.stdout.on('data', function(data){
+  var txt = new Buffer(data).toString('utf8', 0, data.length);
+    io.sockets.send(100 - parseInt(txt.split('  ')[2]));
+});
